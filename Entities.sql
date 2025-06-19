f5ec3a157b4142a08ee7aa5a793e3b24cb09b313
Shakthi Priyanka S <128036146+ShakthipriyankaS24@users.noreply.github.com>
2025-06-19 11:06:30 +0530

    Update Entities.sql

--- Entities.sql
+++ Entities.sql
@@ -48,3 +48,35 @@ CREATE TABLE IF NOT EXISTS Incidents (
     FOREIGN KEY (VictimID) REFERENCES Victims(VictimID),
     FOREIGN KEY (SuspectID) REFERENCES Suspects(SuspectID)
 );
+create table officers (
+    officerid int auto_increment primary key,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
+    badgenumber varchar(20) unique not null,
+    Rank_Details varchar(30),
+    contactinformation varchar(100),
+    agencyid int,
+    foreign key (agencyid) references lawenforcementagencies(agencyid)
+);
+
+create table evidence (
+    evidenceid int auto_increment primary key,
+    description text not null,
+    locationfound varchar(100),
+    incidentid int,
+    foreign key (incidentid) references incidents(incidentid)
+);
+
+create table reports (
+    reportid int auto_increment primary key,
+    incidentid int,
+    reportingofficer int,
+    reportdate datetime,
+    reportdetails text,
+    status enum('draft', 'finalized') default 'draft',
+    foreign key (incidentid) references incidents(incidentid),
+    foreign key (reportingofficer) references officers(officerid)
+);
+
+
+
